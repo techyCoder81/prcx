b5139735c98905bde2dc7a239734e865630eb014
blu-dev <the.blu.dev@gmail.com>
2022-02-19 16:20:03 -0700

    proper diffing

--- src/diff.rs
+++ src/diff.rs
@@ -1,373 +1,152 @@
-use std::path::Path;
+use std::collections::HashMap;
 
-use prc::{
-    ParamKind,
-    ParamStruct,
-    ParamList,
-    hash40::{to_hash40, Hash40}
+use prc::{ParamKind, ParamStruct, ParamList, hash40::Hash40};
+use crate::GetPTag;
+
+use super::{
+    Result,
+    Error,
+    ParamTag
 };
 
-use serde::{
-    Serialize,
-    Deserialize
-};
-
-use crate::{
-    key::{
-        PrcKey,
-        PrcKeyType
-    }
-};
-
-#[derive(Serialize, Deserialize)]
-pub struct Diff(pub Vec<(PrcKey, ParamKind)>);
-
-impl Diff {
-    fn find_key_in_list<'a>(key: &PrcKey, params: &'a mut ParamList) -> Option<&'a mut ParamKind> {
-        let idx = match &key.ty {
-            PrcKeyType::StructField(_) => return None,
-            PrcKeyType::ListIndex(idx) => *idx
-        };
-
-        params.0
-            .get_mut(idx)
-            .map(|x| {
-                if key.next.is_none() {
-                    match x {
-                        ParamKind::Struct(_) => None,
-                        ParamKind::List(_) => None,
-                        x => Some(x)
-                    }
-                } else {
-                    match x {
-                        ParamKind::Struct(s) if key.next.is_some() => {
-                            Self::find_key_in_struct(key.next.as_ref().unwrap().as_ref(), s)
-                        },
-                        ParamKind::List(l) if key.next.is_some() => {
-                            Self::find_key_in_list(key.next.as_ref().unwrap().as_ref(), l)
-                        },
-                        _ => None
-                    }
-                }
-            })
-            .flatten()
-    }
-
-    fn find_key_in_struct<'a>(key: &PrcKey, params: &'a mut ParamStruct) -> Option<&'a mut ParamKind> {
-        let field = match &key.ty {
-            PrcKeyType::StructField(hash) => *hash,
-            PrcKeyType::ListIndex(_) => return None
-        };
-
-        let mut param = None;
-        for (hash, p) in params.0.iter_mut() {
-            if *hash == field {
-                param = Some(p);
-                break;
-            }
-        }
-
-        param
-            .map(|x| {
-                if key.next.is_none() {
-                    match x {
-                        ParamKind::Struct(_) => None,
-                        ParamKind::List(_) => None,
-                        x => Some(x)
-                    }
-                } else {
-                    match x {
-                        ParamKind::Struct(s) if key.next.is_some() => {
-                            Self::find_key_in_struct(key.next.as_ref().unwrap().as_ref(), s)
-                        },
-                        ParamKind::List(l) if key.next.is_some() => {
-                            Self::find_key_in_list(key.next.as_ref().unwrap().as_ref(), l)
-                        },
-                        _ => None
-                    }
-                }
-            })
-            .flatten()
-    }
-
-    fn get_param_kind_from_str(s: &str) -> Option<ParamKind> {
-        if s.starts_with("\"") && s.ends_with("\"") {
-            Some(ParamKind::Str(s.trim_start_matches("\"").trim_end_matches("\"").to_string()))
-        } else if s == "true" {
-            Some(ParamKind::Bool(true))
-        } else if s == "false" {
-            Some(ParamKind::Bool(false))
-        } else if s.starts_with("0x") {
-            let s = s.trim_start_matches("0x");
-            if let Ok(int) = i8::from_str_radix(s, 16) {
-                Some(ParamKind::I8(int))
-            } else if let Ok(int) = u8::from_str_radix(s, 16) {
-                Some(ParamKind::U8(int))
-            } else if let Ok(int) = i16::from_str_radix(s, 16) {
-                Some(ParamKind::I16(int))
-            } else if let Ok(int) = u16::from_str_radix(s, 16) {
-                Some(ParamKind::U16(int))
-            } else if let Ok(int) = i32::from_str_radix(s, 16) {
-                Some(ParamKind::I32(int))
-            } else if let Ok(int) = u32::from_str_radix(s, 16) {
-                Some(ParamKind::U32(int))
+fn find_instance_of(struc: &Vec<(Hash40, ParamKind)>, key: Hash40, count: usize) -> Option<&ParamKind> {
+    let mut current = 0;
+    for (key_, param) in struc.iter() {
+        if key == *key_ {
+            if current == count {
+                return Some(param);
             } else {
-                Hash40::from_hex_str(s).ok().map(|x| ParamKind::Hash(x))
+                current += 1;
             }
-        } else if let Ok(int) = s.parse() {
-            Some(ParamKind::I8(int))
-        } else if let Ok(int) = s.parse() {
-            Some(ParamKind::U8(int))
-        } else if let Ok(int) = s.parse() {
-            Some(ParamKind::I16(int))
-        } else if let Ok(int) = s.parse() {
-            Some(ParamKind::U16(int))
-        } else if let Ok(int) = s.parse() {
-            Some(ParamKind::I32(int))
-        } else if let Ok(int) = s.parse() {
-            Some(ParamKind::U32(int))
-        } else if let Ok(float) = s.parse() {
-            Some(ParamKind::Float(float))
-        } else {
-            Some(ParamKind::Hash(to_hash40(s)))
         }
     }
+    None
+}
 
-    fn find_diffs_in_struct(source: &ParamStruct, modded: &ParamStruct) -> Vec<(PrcKey, ParamKind)> {
-        let mut vec = vec![];
-        for (key, param) in source.0.iter() {
-            let mut modded_param = None;
-            for (m_key, m_param) in modded.0.iter() {
-                if *m_key == *key {
-                    modded_param = Some(m_param);
-                }
-            }
 
-            if let Some (modded_param) = modded_param {
-                if modded_param == param {
-                    continue;
-                }
+pub fn generate_diff_struct(source: &ParamStruct, result: &ParamStruct) -> Result<Option<ParamKind>> {
+    let ParamStruct(source) = source;
+    let ParamStruct(result) = result;
 
-                match param {
-                    ParamKind::Struct(s) => {
-                        if let ParamKind::Struct(s2) = modded_param {
-                            vec.extend(
-                                Self::find_diffs_in_struct(s, s2)
-                                    .into_iter()
-                                    .map(|(x, y)| (PrcKey {
-                                        ty: PrcKeyType::StructField(*key),
-                                        next: Some(Box::new(x))
-                                    }, y))
-                            );
+    let mut vec = vec![];
+    let mut already_encountered = HashMap::new();
+    let mut diffs_encountered = HashMap::new();
+    for (key, param) in result {
+        let count = if let Some(count) = already_encountered.get_mut(key) {
+            *count += 1;
+            *count
+        } else {
+            already_encountered.insert(*key, 0);
+            0
+        };
+
+        if let Some(src_param) = find_instance_of(source, *key, count) {
+            if let Some(diff) = generate_diff(src_param, param)? {
+                if count == 0 {
+                    diffs_encountered.insert(*key, 1usize);
+                    vec.push((*key, diff));
+                } else {
+                    if let Some(diff_count) = diffs_encountered.get_mut(key) {
+                        if count == *diff_count {
+                            *diff_count += 1;
+                            vec.push((*key, diff));
                         } else {
-                            unreachable!()
+                            for x in *diff_count..count {
+                                if let Some(param) = find_instance_of(source, *key, x) {
+                                    vec.push((*key, param.clone()));
+                                } else {
+                                    return Err(Error::BadReturn);
+                                }
+                            }
+                            *diff_count = count + 1;
+                            vec.push((*key, diff));
                         }
-                    },
-                    ParamKind::List(l) => {
-                        if let ParamKind::List(l2) = modded_param {
-                            vec.extend(
-                                Self::find_diffs_in_list(l, l2)
-                                    .into_iter()
-                                    .map(|(x, y)| (PrcKey {
-                                        ty: PrcKeyType::StructField(*key),
-                                        next: Some(Box::new(x))
-                                    }, y))
-                            );
-                        } else {
-                            unreachable!()
+                    } else {
+                        for x in 0..count {
+                            if let Some(param) = find_instance_of(source, *key, x) {
+                                vec.push((*key, param.clone()));
+                            } else {
+                                return Err(Error::BadReturn);
+                            }
                         }
-                    },
-                    _ => {
-                        vec.push((PrcKey {
-                            ty: PrcKeyType::StructField(*key),
-                            next: None
-                        }, modded_param.clone()))
+                        diffs_encountered.insert(*key, count + 1);
+                        vec.push((*key, diff));
                     }
                 }
             }
+        } else {
+            vec.push((*key, param.clone()));
         }
+    }
+    if vec.is_empty() {
+        Ok(None)
+    } else {
+        Ok(Some(ParamKind::Struct(ParamStruct(vec))))
+    }
+}
 
-        vec
+pub fn generate_diff_list(source: &ParamList, result: &ParamList) -> Result<Option<ParamKind>> {
+    let ParamList(source) = source;
+    let ParamList(result) = result;
+    if result.len() < source.len() {
+        return Err(Error::ShortPatchList);
     }
 
-    fn find_diffs_in_list(source: &ParamList, modded: &ParamList) -> Vec<(PrcKey, ParamKind)> {
-        let mut vec = vec![];
-        for (idx, param) in source.0.iter().enumerate() {
-            if let Some(modded_param) = modded.0.get(idx) {
-                if modded_param == param {
-                    continue;
-                }
+    let mut list = vec![];
 
-                match param {
-                    ParamKind::Struct(s) => {
-                        if let ParamKind::Struct(s2) = modded_param {
-                            vec.extend(
-                                Self::find_diffs_in_struct(s, s2)
-                                    .into_iter()
-                                    .map(|(x, y)| (PrcKey {
-                                        ty: PrcKeyType::ListIndex(idx),
-                                        next: Some(Box::new(x))
-                                    }, y))
-                            );
-                        } else {
-                            unreachable!()
-                        }
-                    },
-                    ParamKind::List(l) => {
-                        if let ParamKind::List(l2) = modded_param {
-                            vec.extend(
-                                Self::find_diffs_in_list(l, l2)
-                                    .into_iter()
-                                    .map(|(x, y)| (PrcKey {
-                                        ty: PrcKeyType::ListIndex(idx),
-                                        next: Some(Box::new(x))
-                                    }, y))
-                            );
-                        } else {
-                            unreachable!()
-                        }
-                    },
-                    _ => {
-                        vec.push((PrcKey {
-                            ty: PrcKeyType::ListIndex(idx),
-                            next: None
-                        }, modded_param.clone()))
-                    }
-                }
-            }
-        }
-
-        vec
-    }
-
-    pub fn open<P: AsRef<Path>>(path: P) -> Result<Self, std::io::Error> {
-        let data = std::fs::read_to_string(path)?;
-        let patches = data
-            .lines()
-            .map(|x| {
-                let splits = x.split("=").map(|x| x.trim()).collect::<Vec<&str>>();
-                (splits[0], splits[1])
-            })
-            .filter_map(|(key, value)| {
-                let key: PrcKey = key.parse().unwrap();
-                Self::get_param_kind_from_str(value).map(|x| (key, x))
-            })
-            .collect();
-        Ok(Self(patches))
-    }
-
-    pub fn open_bin<P: AsRef<Path>>(path: P) -> Result<Self, std::io::Error> {
-        let data = std::fs::read(path)?;
-        Ok(bincode::deserialize(&data).unwrap())
-    }
-
-    pub fn save<P: AsRef<Path>>(&self, path: P, is_text: bool) -> Result<(), std::io::Error> {
-        if is_text {
-            let mut file = std::fs::File::create(path)?;
-            for (key, param) in self.0.iter() {
-                use std::io::Write;
-                match param {
-                    ParamKind::Struct(_) => unreachable!(),
-                    ParamKind::List(_) => unreachable!(),
-                    ParamKind::Bool(bool) => { let _ = write!(&mut file, "{} = {}\n", key.to_string(), bool); },
-                    ParamKind::I8(int) => { let _ = write!(&mut file, "{} = {}\n", key.to_string(), int); },    
-                    ParamKind::U8(int) => { let _ = write!(&mut file, "{} = {}\n", key.to_string(), int); },
-                    ParamKind::I16(int) => { let _ = write!(&mut file, "{} = {}\n", key.to_string(), int); },    
-                    ParamKind::U16(int) => { let _ = write!(&mut file, "{} = {}\n", key.to_string(), int); },   
-                    ParamKind::I32(int) => { let _ = write!(&mut file, "{} = {}\n", key.to_string(), int); },    
-                    ParamKind::U32(int) => { let _ = write!(&mut file, "{} = {}\n", key.to_string(), int); },   
-                    ParamKind::Float(float) => { let _ = write!(&mut file, "{} = {}\n", key.to_string(), float); },   
-                    ParamKind::Str(string) => { let _ = write!(&mut file, "{} = \"{}\"\n", key.to_string(), string); },   
-                    ParamKind::Hash(hash) => { let _ = write!(&mut file, "{} = {}\n", key.to_string(), crate::hash::get(*hash)); },   
-                }
+    for (idx, param) in result.iter().enumerate() {
+        if let Some(src_param) = source.get(idx) {
+            if let Some(diff) = generate_diff(src_param, param)? {
+                list.push(diff);
             }
         } else {
-            let data = bincode::serialize(self).unwrap();
-            std::fs::write(path, &data)?;
-        }
-        Ok(())
-    }
-
-    pub fn apply(self, params: &mut ParamStruct) {
-        for (key, value) in self.0 {
-            if let Some(p) = Self::find_key_in_struct(&key, params) {
-                *p = value;
-            }
+            list.push(param.clone());
         }
     }
 
-    pub fn generate(source: &ParamStruct, modded: &ParamStruct) -> Self {
-        Self(Self::find_diffs_in_struct(source, modded))
+    if list.is_empty() {
+        Ok(None)
+    } else {
+        Ok(Some(ParamKind::List(ParamList(list))))
     }
 }
 
-#[test]
-fn write_diff() {
-    let diffs = Diff(vec![
-        ("fighter_param_table[0].landing_attack_air_frame_n".parse().unwrap(), ParamKind::Float(0.0)),
-        ("fighter_param_table[0].landing_attack_air_frame_f".parse().unwrap(), ParamKind::Float(1.0)),
-        ("fighter_param_table[0].landing_attack_air_frame_b".parse().unwrap(), ParamKind::Float(2.0)),
-        ("fighter_param_table[0].landing_attack_air_frame_lw".parse().unwrap(), ParamKind::Float(3.0)),
-        ("fighter_param_table[0].landing_attack_air_frame_hi".parse().unwrap(), ParamKind::Float(4.0)),
-    ]);
+pub fn generate_diff(source: &ParamKind, result: &ParamKind) -> Result<Option<ParamKind>> {
+    if source.get_tag() != result.get_tag() {
+        return Err(Error::NotSamePType);
+    }
 
-    let data = bincode::serialize(&diffs).unwrap();
-    std::fs::write("/home/blujay/dev/arc/prcx/fighter_param_patch.prcx", data).unwrap();
-}
-
-#[test]
-fn read_diff() {
-    hash::add_hashes(vec![
-        "test",
-        "test1",
-        "test2",
-        "test3",
-        "test4",
-        "test5",
-        "test6",
-    ]);
-    let diffs = std::fs::read("/home/blujay/dev/arc/prcx/test.prcx").unwrap();
-    let diffs: Diff = bincode::deserialize(&diffs).unwrap();
-    let test_strs = vec![
-        "test",
-        "test1.test3",
-        "test1.test4",
-        "test2[0].test5",
-        "test2[1].test6",
-    ];
-    for (idx, (path, prm)) in diffs.0.iter().enumerate() {
-        if let ParamKind::Float(f) = prm {
-            assert_eq!(*f, 10.0);
+    if source.get_tag() == ParamTag::Struct {
+        let source = if let ParamKind::Struct(struc) = source {
+            struc
         } else {
-            panic!("ParamKind was not float!");
-        }
-        assert_eq!(path.to_string(), test_strs[idx]);
+            unreachable!()
+        };
+
+        let result = if let ParamKind::Struct(struc) = result {
+            struc
+        } else {
+            unreachable!()
+        };
+
+        generate_diff_struct(source, result)
+    } else if source.get_tag() == ParamTag::List {
+        let source = if let ParamKind::List(list) = source {
+            list
+        } else {
+            unreachable!()
+        };
+
+        let result = if let ParamKind::List(list) = result {
+            list
+        } else {
+            unreachable!()
+        };
+
+        generate_diff_list(source, result)
+    } else if source != result {
+        Ok(Some(result.clone()))
+    } else {
+        Ok(None)
     }
-}
-
-#[test]
-fn apply_diff() {
-    let diffs = Diff(vec![
-        ("fighter_param_table[0].walk_accel_mul".parse().unwrap(), ParamKind::Float(100.0))
-    ]);
-    let mut params = prc::open("/home/blujay/dev/arc/prcx/fighter_param.prc").unwrap();
-    diffs.apply(&mut params);
-    prc::save("/home/blujay/dev/arc/prcx/fighter_param_out.prc", &params).unwrap();
-}
-
-#[test]
-fn read_and_apply_bin_diff() {
-    let diffs = std::fs::read("/home/blujay/dev/arc/prcx/fighter_param_patch.prcx").unwrap();
-    let diffs: Diff = bincode::deserialize(&diffs).unwrap();
-    let mut params = prc::open("/home/blujay/dev/arc/prcx/fighter_param.prc").unwrap();
-    diffs.apply(&mut params);
-    prc::save("/home/blujay/dev/arc/prcx/fighter_param_out.prc", &params).unwrap();
-}
-
-#[test]
-fn read_and_apply_text_diff() {
-    let diffs = Diff::open("/home/blujay/dev/arc/prcx/fighter_param_patch.prctxt").unwrap();
-    let mut params = prc::open("/home/blujay/dev/arc/prcx/fighter_param.prc").unwrap();
-    diffs.apply(&mut params);
-    prc::save("/home/blujay/dev/arc/prcx/fighter_param_out.prc", &params).unwrap();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,99 @@
-pub mod diff;
-pub mod hash;
-pub mod key;
+use thiserror::Error;
 
-pub use prc;
+pub use prc::*;
+
+mod diff;
+
+#[derive(Debug, Error)]
+pub enum Error {
+    #[error("{0}")]
+    IO(#[from] std::io::Error),
+    #[error("A list in the result/patch is shorter than the corresponding list in the source data!")]
+    ShortPatchList,
+    #[error("A param must be the same type between the patch and the source data.")]
+    NotSamePType,
+    #[error("The resulting patch must be a struct!")]
+    BadReturn
+}
+
+#[derive(PartialEq, Eq)]
+enum ParamTag {
+    Bool,
+    I8,
+    U8,
+    I16,
+    U16,
+    I32,
+    U32,
+    Float,
+    Hash,
+    Str,
+    List,
+    Struct
+}
+
+trait GetPTag {
+    fn get_tag(&self) -> ParamTag;
+}
+
+impl GetPTag for ParamKind {
+    fn get_tag(&self) -> ParamTag {
+        match self {
+            ParamKind::Bool(_) => ParamTag::Bool,
+            ParamKind::I8(_) => ParamTag::I8,
+            ParamKind::U8(_) => ParamTag::U8,
+            ParamKind::I16(_) => ParamTag::I16,
+            ParamKind::U16(_) => ParamTag::U16,
+            ParamKind::I32(_) => ParamTag::I32,
+            ParamKind::U32(_) => ParamTag::U32,
+            ParamKind::Float(_) => ParamTag::Float,
+            ParamKind::Hash(_) => ParamTag::Hash,
+            ParamKind::Str(_) => ParamTag::Str,
+            ParamKind::List(_) => ParamTag::List,
+            ParamKind::Struct(_) => ParamTag::Struct,
+        }
+    }
+}
+
+type Result<T> = std::result::Result<T, Error>;
+
+// pub fn apply_patch(patch: &ParamStruct, source: &mut ParamStruct) -> Result<()> {
+
+// }
+
+pub fn generate_patch(source: &ParamStruct, result: &ParamStruct) -> Result<Option<ParamStruct>> {
+    let param = diff::generate_diff_struct(source, result)?;
+    if let Some(param) = param {
+        if let ParamKind::Struct(s) = param {
+            Ok(Some(s))
+        } else {
+            Err(Error::BadReturn)
+        }
+    } else {
+        Ok(None)
+    }
+}
+
+#[test]
+fn generate_patch_test() {
+    let result = open("./tests/hdr_fighter_param.prc").unwrap();
+    let source = open("./tests/vanilla_fighter_param.prc").unwrap();
+    let patch = generate_patch(&source, &result).unwrap().unwrap();
+    save("./tests/hdr_fighter_paramx.prc", &patch).unwrap();
+}
+
+#[test]
+fn generate_double_key_patch_test() {
+    let result = open("./tests/stageparam_metroid_zebesdx_modded.stprm").unwrap();
+    let source = open("./tests/stageparam_metroid_zebesdx.stprm").unwrap();
+    let patch = generate_patch(&source, &result).unwrap().unwrap();
+    save("./tests/stageparam_metroid_zebesdx.stprmx", &patch).unwrap();
+}
+
+#[test]
+fn generate_added_entry_test() {
+    let result = open("./tests/pictochat2_modded.stdat").unwrap();
+    let source = open("./tests/pictochat2.stdat").unwrap();
+    let patch = generate_patch(&source, &result).unwrap().unwrap();
+    save("./tests/pictochat2.stdatx", &patch).unwrap();
+}
